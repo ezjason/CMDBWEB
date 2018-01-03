35f48069cf1f17e5950f4542d4580f75323f5330
wangtao <1615450788@qq.com>
2018-01-03 10:01:36 +0800

    映射表单渲染逻辑

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import routes from './config/router'
 import vuePlugin from './vuePlugin/index'
 import menu from './data/menu'
 import ModuleRender from "./components/table/renderVue.vue";
+import widgetRender from "./components/table/searchItem.vue";
 import TabsRender from "./components/tabs/render.vue";
 import api from './config/api'
 
@@ -12,7 +13,8 @@ export {
     store,
     menu,
     vuePlugin,
+    api,
     ModuleRender,
     TabsRender,
-    api
+    widgetRender,
 }
