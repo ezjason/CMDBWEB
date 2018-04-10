be5638b3b1a31d78594543c955e3cbd14f4d85bf
wangtao <1615450788@qq.com>
2018-04-10 17:48:27 +0800

    $recursion优化返回值为空的情况

--- src/vuePlugin/utils/recursion.js
+++ src/vuePlugin/utils/recursion.js
@@ -2,9 +2,12 @@ export default function recursion(arr,childKey,fn){
     let result=[];
     if(Array.isArray(arr)&&arr.length){
         arr.forEach((val,index)=>{
-            result.push(fn(val)||{});
-            if(Array.isArray(val[childKey])&&val[childKey].length){
-                result[index][childKey]=recursion(val[childKey],childKey,fn)
+            let v=fn(val);
+            if(v){
+                result.push(fn(val));
+                if(Array.isArray(val[childKey])&&val[childKey].length){
+                    result[index][childKey]=recursion(val[childKey],childKey,fn)
+                }
             }
         });
     }
